cc8cc93a8a0038ae879874d5914cb4f1ebc26ffe
GUERIN MATHIS p2105770 <mathis.guerin@etu.univ-lyon1.fr>
2023-04-07 17:33:17 +0200

    update api

--- api-bonk/index.ts
+++ api-bonk/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import { createServer } from 'http';
 import api from './api';
 import { connect } from './api/config/database';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', api);
 
