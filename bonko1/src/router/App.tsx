4d5619678a9c0bf322b2bf6fa084ef28ea4403b9
GUERIN MATHIS p2105770 <mathis.guerin@etu.univ-lyon1.fr>
2023-04-07 16:11:17 +0200

    router and navar

--- bonko1/src/router/App.tsx
+++ bonko1/src/router/App.tsx
@@ -6,15 +6,37 @@ import {
   Link
 } from "react-router-dom";
 import Mock from '../pages/Mock/Mock';
+import Login from '../pages/login/Login';
+import Register from '../pages/register/Register';
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/new">New announce</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
+        <Route path={'/login'} element={<Login />} />
+        <Route path={'/register'} element={<Register />} />
         <Route path={'/'} element={<Mock />} />
-        <Route path={'/login'} element={<Mock />} />
         <Route path={'/profile'} element={<Mock />} />
-        <Route path={'/callback'} element={<Mock />} />
+        <Route path={'/new'} element={<Mock />} />
       </Routes>    
     </Router>
   );
